52b32425148505f52a3d8423fd2fcd65fd1acbd0
jusalfort07 <78145003+jusalfort07@users.noreply.github.com>
2023-11-10 08:13:04 -0700

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
-import { setLocalStorage, getParam } from './utils.mjs';
+import { setLocalStorage, getParams } from './utils.mjs';
 
-const productId = getParam('product');
+const productId = getParams('product');
 const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
