8bddd77aa21203c6bc0f26ca56011c2040f8a3f6
Alfred Ortega <jusalfort.07@gmail.com>
2023-11-12 03:28:25 +0800

    fix netlify and double quotes

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { setLocalStorage, getParams } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParams } from "./utils.mjs";
 
-const productId = getParams('product');
-const dataSource = new ProductData('tents');
+const productId = getParams("product");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
