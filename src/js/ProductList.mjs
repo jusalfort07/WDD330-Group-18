c215d1b1d4f67437f167e44eb5db0caccac5922c
Alfred Ortega <jusalfort.07@gmail.com>
2023-11-12 05:38:59 +0800

    Individual Activity 2

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,41 @@
+import { renderListWithTemplate } from "./utils.mjs";   
+
+function productCardTemplate(product){
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+      <img
+        src="${product.Image}"
+        alt="${product.NameWithoutBrand}"
+      />
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">$${product.FinalPrice}</p></a
+    >
+  </li>`
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement){
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+
+    }
+    async init() {
+        const list = await this.dataSource.getData();
+        const limitedList = this.limitResult(list);
+
+        this.renderList(limitedList);
+    }
+    renderList(list) {
+        // const htmlStrings = list.map(productCardTemplate);
+        // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+
+    //limits the result to 4
+    limitResult(list){
+        return list.splice(0,4)
+    }
+}
