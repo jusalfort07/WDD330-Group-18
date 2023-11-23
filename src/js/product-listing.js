650d8ad1946d4b5ee2ae08b8b95026df56ea60a9
Alfred Ortega <jusalfort.07@gmail.com>
2023-11-23 23:49:30 +0800

    Week 04 Team Activity

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,9 +1,9 @@
-import ProductData from "./ProductData.mjs"
+import ExternalServices from "./ExternalServices.mjs"
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParams } from "./utils.mjs";
 
 const category = getParams("category");
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const listElement = document.querySelector(".product-list");
 const list = new ProductList(category, dataSource, listElement)
 
